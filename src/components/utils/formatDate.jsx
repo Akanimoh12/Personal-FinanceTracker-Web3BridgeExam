7d0657f7f53179f9e4f4d8b8f0afcd8ce90d9d6d
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-05-14 15:33:09 +0100

    Updated my Chart for more

--- src/components/utils/formatDate.jsx
+++ src/components/utils/formatDate.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-
-export default function formatDate() {
-  return (
-    <div>formatDate</div>
-  )
+export function formatDate(dateString) {
+  return new Date(dateString).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  });
 }
